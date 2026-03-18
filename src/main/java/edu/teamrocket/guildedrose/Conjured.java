17a9445922beb923f9a3b739ed15d3171b1c6bb4
Juan071825 <juanmateoalvarez@gmail.com>
2026-03-18 14:20:53 +0100

    feat(Conjured.java): :sparkles: Implementar clase Conjured.

---
+++ src/main/java/edu/teamrocket/guildedrose/Conjured.java
@@ -0,0 +1,35 @@
+package edu.teamrocket.guildedrose;
+
+public class Conjured extends NormalItem{
+
+    public Conjured (String name, int SellIn, int quality){
+        super(name, SellIn, quality);
+    }
+
+    @Override
+    public void updateQuality() {
+
+        if (getSellIn() > 0){
+            computeQuality(-2);
+        } else {
+            computeQuality(-4);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
