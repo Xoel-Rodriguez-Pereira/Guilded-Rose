f6c01335c9ea7503c0bbb472e544550d243d2619
Xoel-Rodriguez-Pereira <xoelrodriguezpereira@gmail.com>
2026-03-18 14:00:57 +0100

    feat(AgedBrie): Añadir clase AgedBrie
    Sobreescribir metodo updateQuality para la historia de usuario de AgedBrie

---
+++ src/main/java/edu/teamrocket/guildedrose/AgedBrie.java
@@ -0,0 +1,17 @@
+package edu.teamrocket.guildedrose;
+
+public class AgedBrie extends NormalItem {
+
+    public AgedBrie(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    public void updateQuality() {
+        if (getSellIn() > 0) {
+            computeQuality(+1);
+        } else {
+            computeQuality(+2);
+        }
+        setSellIn();
+    }
+}
