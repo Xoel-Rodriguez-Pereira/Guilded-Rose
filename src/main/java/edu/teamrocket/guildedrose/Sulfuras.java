216712b4bb1b63b05d55db96b2d332f1b2f6f5dc
juan071825 <juanmateoalvarez@gmail.com>
2026-03-20 13:57:33 +0100

    feat(Sulfuras.java): :sparkles: Añadir clase Sulfuras con sus casos test.

---
+++ src/main/java/edu/teamrocket/guildedrose/Sulfuras.java
@@ -0,0 +1,11 @@
+package edu.teamrocket.guildedrose;
+
+public class Sulfuras extends NormalItem{
+    public Sulfuras (String name, int SellIn, int quality){
+    super(name, SellIn, quality);
+    }
+
+    @Override
+    public void updateQuality(){};
+    }
+
